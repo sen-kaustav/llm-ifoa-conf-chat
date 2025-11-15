a23f4737e5608902bdae9a5700ee6abd4fb70761
sen-kaustav <sen_kaustav@hotmail.com>
2025-11-15 17:57:07 +0000

    reformatted the RAG store script and added store title

--- scripts/02_create_RAG_store.R
+++ scripts/02_create_RAG_store.R
@@ -5,17 +5,19 @@ library(DBI)
 
 df_sessions <- read_csv("data/session.csv")
 
-# setup a duckdb database
-con <- DBI::dbConnect(duckdb(), "data/ifoa_session.ragnar.duckdb")
-
 # Store location
 store_location <- file.path("data", "ifoa_session.ragnar.duckdb")
+
+# setup a duckdb database
+con <- DBI::dbConnect(duckdb(), store_location, read_only = FALSE)
 
 store <- ragnar_store_create(
   location = store_location,
   embed = embed_google_gemini(),
+  title = "IFoA Life Conference 2025 schedule",
   overwrite = TRUE,
   version = 1
 )
 
 ragnar_store_insert(store, df_sessions)
+dbDisconnect(con)
